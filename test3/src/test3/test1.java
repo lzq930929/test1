9cde55c5411a751a53d5edbbbeb41dce1c4fb241
Administrator <lzq930929@163.com>
2019-05-31 16:28:04 +0800

    411

--- test3/src/test3/test1.java
+++ test3/src/test3/test1.java
@@ -5,9 +5,6 @@ public class test1 {
 		System.out.println("cccc");
 		System.out.println("476874");
 		System.out.println("84798");
-		System.out.println("7");
-		System.out.println("757485");
-		System.out.println("4768756454");
-		System.out.println("4768756454");
+
 	}
 }
