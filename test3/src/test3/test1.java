957f8b940623dd63d40c0808149323209ab8a61a
Administrator <lzq930929@163.com>
2019-05-31 16:24:35 +0800

    12

--- test3/src/test3/test1.java
+++ test3/src/test3/test1.java
@@ -7,5 +7,7 @@ public class test1 {
 		System.out.println("84798");
 		System.out.println("7");
 		System.out.println("757485");
+		System.out.println("4768756454");
+		System.out.println("4768756454");
 	}
 }
